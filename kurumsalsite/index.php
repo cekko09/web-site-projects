fdadf31d1dc9e6c5345e5fb87a1ac0bf39eab9ca
Cenk Yalçın <35816893+cekko09@users.noreply.github.com>
2023-03-17 20:36:11 +0300

    Add files via upload

---
+++ kurumsalsite/index.php
@@ -0,0 +1,431 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta charset="utf-8">
+  <title>DENTİST</title>
+
+
+  <!-- Fontlar -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+  <!-- Bootstrap stil dosyası -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- işimize yarayacak diğer kütüphane css dosyalarımız -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="owl-carousel/owl.theme.css">
+  <script src="jquery-1.9.1.min.js"></script>
+  <script src="assets/owl-carousel/owl.carousel.js"></script>
+
+  <!-- bizim stil dosyamız -->
+  <link href="css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+  <!-- UST BAR -->
+
+
+  <section id="topbar" class="d-none d-lg-block">
+    <div class="container clearfix">
+
+      <div class="contact-info float-left">
+
+        <i class="fa fa-envelope-o"></i><a href="">info@ıphonexdental.com</a>
+        <i class="fa fa-phone"></i>507 490 97 90
+
+      </div>
+      <div class="social-links float-right">
+        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+        <a href="#" class="twitter"><i class="fa fa-facebook"></i></a>
+        <a href="#" class="twitter"><i class="fa fa-instagram"></i></a>
+
+
+      </div>
+
+
+    </div>
+
+
+
+  </section>
+
+
+  <!-- HEADER -->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="#body" class="scrollto">IphoneX <span>Dental</span></a> </h1>
+      </div>
+
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#body">Anasayfa</a></li>
+          <li><a href="#hakkimizda">Hakkımızda</a></li>
+          <li><a href="#hizmetler">Hizmetler</a></li>
+          <li><a href="#iletisim">İletişim</a></li>
+        </ul>
+      </nav>
+
+    </div>
+
+
+
+  </header>
+  <!-- INTRO -->
+
+  <section id="intro">
+
+    <div class="intro-content">
+      <h2>IphoneX Dental Güvencesi</h2>
+
+    </div>
+    <div id="intro-carousel" class="owl-carousel">
+
+      <div class="item" style="background-image: url(img/imgd/gazi-hastanesi-ortopedi-ve-travmatoloji.png);"></div>
+      <div class="item" style="background-image: url(img/imgd/HİZMETLER-ortopedi.png);"></div>
+      <div class="item" style="background-image: url(img/imgd/ortopedi-ve-travmatoloji.png);"></div>
+      <div class="item" style="background-image: url(img/imgd/backgroundstetescope.png);"></div>
+
+    </div>
+
+  </section>
+  <!-- hakkımızda MAİN -->
+  <main id="main">
+    <section id="hakkimizda" class="wow fadeInUp">
+
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6 hakkimizda-img">
+            <img src="img/hakkimiz.jpg" alt="hakkimizda">
+          </div>
+          <div class="col-lg-6 content">
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, voluptatem?</h2>
+            <h3>Lorem ipsum Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, quaerat. dolor sit, amet consectetur adipisicing elit. Animi minus recusandae incidunt alias obcaecati et fugiat dolorem tempore quas! Optio.</h3>
+          </div>
+        </div>
+
+      </div>
+
+
+    </section>
+    <!-- hizmetler MAİN -->
+    <section class="" id="hizmetler">
+      <div class="container">
+        <div class="section-header">
+          <h2>HİZMETLERİMİZ</h2>
+          <p>Yılların vermiş oldugu tecrube ile urunlerinizi güvenle taşırız</p>
+
+        </div>
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="box wow fadeInLeft">
+              <div class="icon"><i class="fa fa-bar-chart"></i> </div>
+              <h4 class="title"><a href="#">Lorem Ipsum</a></h4>
+              <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, iure!</p>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="box wow fadeInRight">
+              <div class="icon"><i class="fa fa-bar-chart"></i> </div>
+              <h4 class="title"><a href="#">Lorem Ipsum</a></h4>
+              <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, iure!</p>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="box wow fadeInLeft">
+              <div class="icon"><i class="fa fa-bar-chart"></i> </div>
+              <h4 class="title"><a href="#">Lorem Ipsum</a></h4>
+              <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, iure!</p>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="box wow fadeInRight">
+              <div class="icon"><i class="fa fa-bar-chart"></i> </div>
+              <h4 class="title"><a href="#">Lorem Ipsum</a></h4>
+              <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, iure!</p>
+            </div>
+          </div>
+
+        </div>
+    </section>
+    <!-- REFERANSLAR -->
+    
+
+    <!-- FİLOMUZ -->
+
+    <section id="filo" class="wow fadeInUp">
+
+      <div class="container">
+
+        <div class="section-header">
+          <h2>ÇALIŞMALARIMIZ</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque expedita vel officia, pariatur porro rerum!</p>
+
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="row no-gutters">
+          <div class="col-lg-3 col-md-4">
+            <div class="filo-item wow fadeInUp">
+              <a style="height:300px;" href="img/imgd/dis-beyazlatma.jpg" class="filo-popup">
+                <img style="height:300px;" src="img/imgd/dis-beyazlatma.jpg" alt="">
+                <div class="filo-overlay"></div>
+                <div class="filo-info">
+                  <h2 class="wow fadeInUp"></h2>
+                </div>
+
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4">
+            <div class="filo-item wow fadeInUp">
+              <a style="height:300px;" href="img/imgd/dis-renginde-sararma-neden-olur_16_9_1597305486-880x495.jpg" class="filo-popup">
+                <img style="height:300px;" src="img/imgd/dis-renginde-sararma-neden-olur_16_9_1597305486-880x495.jpg" alt="">
+                <div class="filo-overlay"></div>
+                <div class="filo-info">
+                  <h2 class="wow fadeInUp"></h2>
+                </div>
+
+              </a>
+            </div>
+          </div>
+         
+          <div class="col-lg-3 col-md-4">
+            <div class="filo-item wow fadeInUp">
+              <a style="height:300px;" href="img/imgd/tartar-diş-taşı.jpg" class="filo-popup">
+                <img style="height:300px;" src="img/imgd/tartar-diş-taşı.jpg" alt="">
+                <div class="filo-overlay"></div>
+                <div class="filo-info">
+                  <h2 class="wow fadeInUp"></h2>
+                </div>
+
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4">
+            <div class="filo-item wow fadeInUp">
+              <a style="height:300px;" href="img/imgd/tartar-diş-taşı.jpg" class="filo-popup">
+                <img style="height:300px;" src="img/imgd/tartar-diş-taşı.jpg" alt="">
+                <div class="filo-overlay"></div>
+                <div class="filo-info">
+                  <h2 class="wow fadeInUp"></h2>
+                </div>
+
+              </a>
+            </div>
+          </div>
+         
+         
+        </div>
+      </div>
+
+
+
+
+
+    </section>
+
+    <!-- MUSTERİ YORUMLAR -->
+
+
+    <section id="yorumlar" class="wow fadeInUp">
+      <div class="container">
+
+        <div class="section-header">
+          <h2>Ekibimiz</h2>
+          <p>Lorem, ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, vero. sit amet consectetur adipisicing elit. Eaque expedita vel officia, pariatur porro rerum!</p>
+
+        </div>
+
+        <div class=" owl-carousel testimonials-carousel">
+
+          <div class="testimonial-item">
+            <p>
+              <img src="img/sol.png" class="quote-sign-left" alt="">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, sequi.
+              <img src="img/sol.png" class="quote-sign-right" alt="">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, magnam.
+            </p>
+            <img src="img/imgd/enes.jpg" class="testimonial-img" alt="">
+            <h3>isim</h3>
+          </div>
+
+          <div class="testimonial-item">
+            <p>
+              <img src="img/sol.png" class="quote-sign-left" alt="">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, sequi.
+              <img src="img/sol.png" class="quote-sign-right" alt="">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, magnam.
+            </p>
+            <img src="img/imgd/cengşz.jpg" class="testimonial-img" alt="">
+            <h3>isim</h3>
+          </div>
+
+          <div class="testimonial-item">
+            <p>
+              <img src="img/sol.png" class="quote-sign-left" alt="">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, sequi.
+              <img src="img/sol.png" class="quote-sign-right" alt="">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, magnam.
+            </p>
+            <img src="img/imgd/enes.jpg" class="testimonial-img" alt="">
+            <h3>isim</h3>
+          </div>
+          <div class="testimonial-item">
+            <p>
+              <img src="img/sol.png" class="quote-sign-left" alt="">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, sequi.
+              <img src="img/sol.png" class="quote-sign-right" alt="">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, magnam.
+            </p>
+            <img src="img/imgd/cengşz.jpg" class="testimonial-img" alt="">
+            <h3>isim</h3>
+          </div>
+          <div class="testimonial-item">
+            <p>
+              <img src="img/sol.png" class="quote-sign-left" alt="">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, sequi.
+              <img src="img/sol.png" class="quote-sign-right" alt="">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, magnam.
+            </p>
+            <img src="img/imgd/enes.jpg" class="testimonial-img" alt="">
+            <h3>isim</h3>
+          </div>
+          <div class="testimonial-item">
+            <p>
+              <img src="img/sol.png" class="quote-sign-left" alt="">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, sequi.
+              <img src="img/sol.png" class="quote-sign-right" alt="">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, magnam.
+            </p>
+            <img src="img/imgd/cengşz.jpg" class="testimonial-img" alt="">
+            <h3>isim</h3>
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+
+
+
+    </section>
+    <!-- İLETİŞİM -->
+
+    <section class="wow fadeInUp" id="iletisim">
+
+      <div class="container">
+
+        <div class="section-header">
+          <h2>İletişim</h2>
+          <p>Lorem, ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, vero. sit amet consectetur adipisicing elit. Eaque expedita vel officia, pariatur porro rerum!</p>
+
+        </div>
+
+      </div>
+
+      <div class="row contact-info">
+        <div class="col-md-4">
+          <div class="contact-adress">
+            <i class="ion-ios-location-outline"></i>
+            <h3>Adresimiz</h3>
+            <address>evren mah. dunya sokak kabugu apt. no:1</address>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="contact-phone">
+            <i class="ion-ios-telephone-outline"></i>
+            <h3>Telefon</h3>
+            <p><a href="tel:+905074909790">507 490 97 90</a></p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="contact-email">
+            <i class="ion-ios-email-outline"></i>
+            <h3>Adresimiz</h3>
+            <p><a href="mailto:cekkooo09@gmail.com">cekkooo09@gmail.com</a></p>
+          </div>
+        </div>
+
+
+      </div>
+
+      <div class="container mb-4">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96374.63275479004!2d28.576593133846714!3d40.98789030488987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14b55fc19deb0b3b%3A0xdf4ea093f30983c6!2zQmV5bGlrZMO8esO8L8Swc3RhbmJ1bA!5e0!3m2!1str!2str!4v1630343661027!5m2!1str!2str" width="100%" height="380" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+      </div>
+
+      <div class="container">
+        <div class="form">
+          <div id="mesajsonuc"></div>
+          <div id="mesajhata"></div>
+          <form id="mailform">
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <input type="text" name="isim" class="form-control" placeholder="adınız" required="required">
+              </div>
+              <div class="form-group col-md-6">
+                <input type="mail" name="mail" class="form-control" placeholder="mail adresiniz" required="required">
+              </div>
+            </div>
+            <div class="form-group">
+              <input type="text" name="konu" class="form-control" placeholder="konu" required="required">
+            </div>
+            <div class="form-group">
+              <textarea name="mesaj" id="mesaj" class="form-control " rows="5"></textarea>
+            </div>
+            <div class="text-center">
+              <input type="button" value="gönder" class="btn btn-info rounded" id="gonderbtn">
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </section>
+
+  </main>
+
+  <!-- footer -->
+
+  <footer id="footer">
+
+    <div class="container">
+      <div class="copyright">2019 &copy; Copyright <strong></strong></div>
+      <div class="credits">
+
+      </div>
+    </div>
+
+  </footer>
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <!-- Kütüphaneler -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="lib/sticky/sticky.js"></script>
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
